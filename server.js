7378c91e2e26c46c1d0827e681d5fad420bbe02a
ofirbal993 <ofirbal933@gmail.com>
2025-07-30 23:07:05 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const http = require('http');
+
+const hostname = '127.0.0.1'; // or '0.0.0.0' to accept external requests
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Node.js server!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
