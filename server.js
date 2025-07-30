fe8674702d3afd76e6ec1b02f75142dc9745397a
ofirbal993 <ofirbal933@gmail.com>
2025-07-30 23:15:13 +0300

    Update server.js -set port to 10000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1'; // or '0.0.0.0' to accept external requests
-const port = 3000;
+const port = 10000;
 
 const server = http.createServer((req, res) => {
   console.log(`Request: ${req.method} ${req.url}`);
