a3263c9bc0c1a9cc3b597b1596809fb5f990f067
ofirbal993 <ofirbal933@gmail.com>
2025-07-30 23:19:19 +0300

    Update server.js - hostname = '0.0.0.0'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const http = require('http');
 
-const hostname = '127.0.0.1'; // or '0.0.0.0' to accept external requests
+const hostname = '0.0.0.0'; // or '0.0.0.0' to accept external requests
 const port = 10000;
 
 const server = http.createServer((req, res) => {
